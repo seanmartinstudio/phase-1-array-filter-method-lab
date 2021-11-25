9415512f5a7b8836ab808d7ca8ce1b453e8e24c5
Sean Martin <sean@seanmartinstudio.com>
2021-11-24 17:45:23 -0800

    commit tree d5a5b7e28025d1819c9d570797cb35ccda43304a

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution here
+
+//1 Before Refactor
+// function findMatching(array, string) {
+//     let newArray = []
+//     for(let item of array) {
+//         if (item.toLowerCase() === string.toLowerCase()) {
+//             newArray.push(item)
+//         }
+//     }
+//     return newArray
+// }
+
+
+// //1
+// function findMatching(array, string) {
+//     function itemFunction (item) {
+//         return item.toLowerCase() === string.toLowerCase()
+//     }
+//     return array.filter(itemFunction)
+//   }
+
+
+
+
+
+
+
+  //1
+function findMatching(array, string) {
+  return array.filter (item => item.toLowerCase() === string.toLowerCase())
+}
+
+  //2
+function fuzzyMatch(array, string) {
+return array.filter (item => item.toLowerCase().indexOf(string.toLowerCase()) === 0)
+}
+
+//3
+function matchName(array, string) {
+return array.filter (item => item.name.toLowerCase() === string.toLowerCase())
+}
